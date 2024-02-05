5f0b23668fd77cd315f1b03dd9c76976e7f004e8
AlexIT <alexit81@yandex.ru>
2024-02-05 11:57:36 +0300

    feat: доработать интерфейс навигации

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -4,7 +4,6 @@ import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import PropTypes from 'prop-types';
 import styles from './burger-ingredients.module.css';
 import { IngredientsList } from '../ingredients-list/ingredients-list';
-import { ingredientPropTypes } from '../../utils/prop-types';
 import { getInrgedients } from '../../services/actions/ingredients';
 import { getInrgedientsSelector } from '../../utils/constants';
 
@@ -23,6 +22,7 @@ export const BurgerIngredients = ({ handleModalIngredient }) => {
   const mainArr = ingredients.filter((item) => item.type === 'main');
 
   // Прокрутка
+  const containerRef = useRef(null);
   const bunRef = useRef(null);
   const sauceRef = useRef(null);
   const mainRef = useRef(null);
@@ -45,42 +45,53 @@ export const BurgerIngredients = ({ handleModalIngredient }) => {
     }
   }, [current]);
 
+  // прокрутка с подсветкой
+  const handleScroll = () => {
+    const bunRefLength = Math.abs(containerRef.current.offsetTop - bunRef.current.getBoundingClientRect().top);
+    const sauceRefLength = Math.abs(containerRef.current.offsetTop - sauceRef.current.getBoundingClientRect().top);
+    const mainRefLength = Math.abs(containerRef.current.offsetTop - mainRef.current.getBoundingClientRect().top);
+    // console.log(`${bunRefLength}  | ${sauceRefLength} | ${mainRefLength}`);
+    if (bunRefLength < sauceRefLength && bunRefLength < mainRefLength) {
+      setCurrent('bun');
+    } else if (sauceRefLength < mainRefLength && sauceRefLength < bunRefLength) {
+      setCurrent('sauce');
+    } else {
+      setCurrent('main');
+    }
+  };
+
   return (
-    <section className='pt-10'>
-      <h1 className='mb-5 text text_type_main-large'>Соберите бургер</h1>
-      <nav className='mb-10'>
+    <section className="pt-10">
+      <h1 className="mb-5 text text_type_main-large">Соберите бургер</h1>
+      <nav className="mb-10">
         <ul className={styles['ingredients-menu']}>
           <li>
-            <Tab value='bun' active={current === 'bun'} onClick={setCurrent}>
+            <Tab value="bun" active={current === 'bun'} onClick={setCurrent}>
               Булки
             </Tab>
           </li>
           <li>
-            <Tab
-              value='sauce'
-              active={current === 'sauce'}
-              onClick={setCurrent}
-            >
+            <Tab value="sauce" active={current === 'sauce'} onClick={setCurrent}>
               Соусы
             </Tab>
           </li>
           <li>
-            <Tab value='main' active={current === 'main'} onClick={setCurrent}>
+            <Tab value="main" active={current === 'main'} onClick={setCurrent}>
               Начинки
             </Tab>
           </li>
         </ul>
       </nav>
-      <div className={`${styles['cards-wrapper']} scrolling`}>
-        <h2 className='text text_type_main-medium' ref={bunRef}>
+      <div className={`${styles['cards-wrapper']} scrolling`} onScroll={handleScroll} ref={containerRef}>
+        <h2 className="text text_type_main-medium" ref={bunRef}>
           Булки
         </h2>
         <IngredientsList ingredients={bunArr} handleModalIngredient={handleModalIngredient} />
-        <h2 className='text text_type_main-medium' ref={sauceRef}>
+        <h2 className="text text_type_main-medium" ref={sauceRef}>
           Соусы
         </h2>
         <IngredientsList ingredients={sauceArr} handleModalIngredient={handleModalIngredient} />
-        <h2 className='text text_type_main-medium' ref={mainRef}>
+        <h2 className="text text_type_main-medium" ref={mainRef}>
           Начинки
         </h2>
         <IngredientsList ingredients={mainArr} handleModalIngredient={handleModalIngredient} />
